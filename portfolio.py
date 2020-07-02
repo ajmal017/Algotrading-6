5aac9015f8c2493acb046f8e503b0797d41affab
mattkaneb1 <matthewkaneb1@gmail.com>
2020-07-01 20:29:40 -0400

    Almost done w algo framework

---
+++ portfolio.py
@@ -0,0 +1,35 @@
+import datetime
+import numpy as np
+import pandas as pd
+import Queue
+
+from abc import ABCMeta, abstractmethod
+from math import floor
+
+from event import FillEvent, OrderEvent
+
+class Portfolio(object):
+    """
+    The Portfolio class handles the positions and market
+    value of all instruments at a resolution of a "bar",
+    i.e. secondly, minutely, 5-min, 30-min, 60 min or EOD.
+    """
+
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def update_signal(self, event):
+        """
+        Acts on a SignalEvent to generate new orders 
+        based on the portfolio logic.
+        """
+        raise NotImplementedError("Should implement update_signal()")
+
+    @abstractmethod
+    def update_fill(self, event):
+        """
+        Updates the portfolio current positions and holdings 
+        from a FillEvent.
+        """
+        raise NotImplementedError("Should implement update_fill()")
+   	
